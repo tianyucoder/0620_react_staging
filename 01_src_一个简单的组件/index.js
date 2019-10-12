5a38dacbbe7b91b5adf5d61394d899fee14ad61b
zhangtianyu <zhangtianyu5177@sina.com>
2019-10-12 09:42:23 +0800

    init

---
+++ 01_src_一个简单的组件/index.js
@@ -0,0 +1,8 @@
+/*
+* 入口js:1.React核心库，2.引入ReactDOM 3.引入App
+* */
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
--- src/index.js
+++ src/index.js
@@ -1,8 +1,5 @@
-/*
-* 入口js:1.React核心库，2.引入ReactDOM 3.引入App
-* */
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from 'react'
+import ReactDOM from 'react-dom'
+import App from './App'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App/>,document.getElementById('root'))
